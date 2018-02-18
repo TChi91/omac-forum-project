e60f02ac695f0f247f803e39394ad08244fffe38
Fethi TChi <adm4algnw@gmail.com>
2018-02-18 18:12:39 +0100

    models and main file created

---
+++ forums/main.py
@@ -0,0 +1,8 @@
+from models import *
+
+member1 = Member("fethi", 25)
+member2 = Member("Ahmed", 33)
+
+post1 = Post("Post one", "Hello from Post one")
+post2 = Post("Post Two", "Hello from Post Two")
+post3 = Post("Post Three", "Hello from Post Three")
