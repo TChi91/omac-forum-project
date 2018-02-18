c69eae6c27b41e7d917e17027b5eb05133540bd5
Fethi TChi <adm4algnw@gmail.com>
2018-02-18 13:20:42 +0100

    re-writing the solution

--- atm.py
+++ atm.py
@@ -1,25 +1,27 @@
 money = 500
 request = 275
 if request > money:
-    print "we can't give more than 500$"
+    print "we can't give more than" + str(money)
+elif request < 0:
+    print "request is not valide"
 else:
     while request > 0:
         if request >= 100:
-            request -= 100
             print "give 100"
+            request -= 100
 
-        if request >= 50 and request < 100:
-            request -= 50
+        elif request >= 50:
             print "give 50"
+            request -= 50
 
-        if request >= 10 and request < 50:
-            request -= 10
+        elif request >= 10:
             print "give 10"
+            request -= 10
 
-        if request >= 5 and request < 10:
-            request -= 5
+        elif request >= 5:
             print "give 5"
+            request -= 5
 
-        if request < 5 and request > 0:
+        elif request < 5:
             print "give " + str(request)
             request -= request
